c28842ff027f5c8c7954d20930cda36041ce8ad9
Sahas <sahasmcg2995@gmail.com>
2024-10-25 18:41:37 +0530

    Initial Commit

---
+++ network-image/network_imageApp.swift
@@ -0,0 +1,17 @@
+//
+//  network_imageApp.swift
+//  network-image
+//
+//  Created by Sahas Punchihewa on 2024-10-25.
+//
+
+import SwiftUI
+
+@main
+struct network_imageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
